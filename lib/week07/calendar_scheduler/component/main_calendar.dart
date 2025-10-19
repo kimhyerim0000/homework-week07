386238712e29fa4352965a67f70e496076dad7c4
kimhyerim <kimhyyefla@gmail.com>
2025-10-19 20:28:52 +0900

    feat: Implement MainCalendar widget skeleton

--- lib/week07/calendar_scheduler/component/main_calendar.dart
+++ lib/week07/calendar_scheduler/component/main_calendar.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
+
+class MainCalendar extends StatelessWidget {
+  final OnDaySelected onDaySelected;
+  final DateTime selectedDate;
+
+  const MainCalendar({
+    super.key,
+    required this.onDaySelected,
+    required this.selectedDate,
+  });
+}
