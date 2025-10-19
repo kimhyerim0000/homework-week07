505b9a4c4377acdc0b99ac0321d1cbd3d5047565
kimhy <kimhyyefla@gmail.com>
2025-10-19 20:59:40 +0900

    feat(schedule_bottom_sheet): Add basic UI skeleton

--- lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -18,8 +18,11 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
     return SafeArea(
       child: Container(
         height: MediaQuery.of(context).size.height / 2 + bottomInset,
-        
-      )
-    )
+        color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.only(left: 8, right: 8, top: 8
+        )
+      ),
+    );
   }
 }
