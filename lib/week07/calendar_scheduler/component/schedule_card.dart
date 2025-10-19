b275e9d68cc313b426ba089b6ef7488331a8ee7e
kimhy <kimhyyefla@gmail.com>
2025-10-19 21:09:40 +0900

    feat: Implement initial schedule card component

--- lib/week07/calendar_scheduler/component/schedule_card.dart
+++ lib/week07/calendar_scheduler/component/schedule_card.dart
@@ -1,1 +1,12 @@
-import ''
+import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class _Time extends StatelessWidget {
+  final int startTime; // 시작 시간
+  final int endTime;   // 종료 시간
+
+  const _Time({
+    required this.startTime,
+    required this.endTime,
+  })
+}
