6c54bd44f052210ca4a53008514ec450ebde8c7f
kimhy <kimhyyefla@gmail.com>
2025-10-20 00:34:05 +0900

    Fix: Remove trailing import statement.

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:myapp/week07/calendar_scheduler/component/today_banner.dart';
 import 'package:myapp/week07/calendar_scheduler/component/schedule_card.dart';
 import 'package:myapp/week07/calendar_scheduler/component/main_calendar.dart';
 import 'package:flutter/material.dart';
+import
 
 // StatelessWidget에서 StatefulWidget으로 전환
 class HomeScreen extends StatefulWidget {
