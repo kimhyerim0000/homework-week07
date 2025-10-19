30e025b0758dc00ce74ce3d22d300d6423880dd0
kimhyerim <kimhyyefla@gmail.com>
2025-10-19 20:02:58 +0900

    feat(schedule): Add column descriptions to Schedule table

--- lib/week07/calendar_scheduler/model/schedule.dart
+++ lib/week07/calendar_scheduler/model/schedule.dart
@@ -1,9 +1,9 @@
 import 'package:drift/drift.dart';
 
 class Schedules extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get content => text()();
-  DateTimeColumn get date => dateTime()();
-  IntColumn get startTime => integer()();
-  IntColumn get endTime => integer()();
+  IntColumn get id => integer().autoIncrement()(); // PRIMARY KEY, 정수 열
+  TextColumn get content => text()(); // 내용, 글자 열
+  DateTimeColumn get date => dateTime()(); // 일정 날짜, 날짜 열
+  IntColumn get startTime => integer()(); // 시작 시간
+  IntColumn get endTime => integer()(); // 종료 시간
 }
