7967aa7a2a159d8c8b6ed9f28ce1623c67c72b18
kimhy <kimhyyefla@gmail.com>
2025-10-20 00:18:35 +0900

    feat(calendar_scheduler): Implement time input validation in schedule bottom sheet

--- lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -94,7 +94,22 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
 
   void onSavePressed() {}
 
-  String? timeValidator(String? val) {} // 시간값 검증
+  String? timeValidator(String? val) {
+    // 시간 검증 함수
+    if (val == null) {
+      return '값을 입력해주세요';
+    }
+    int? number;
+
+    try {
+      number = int.parse(val);
+    } catch (e) {
+      return '숫자를 입력해주세요';
+    }
+    if (number <0 || number > 24) {
+      
+    }
+  }
 
   String? contentValidator(String? val) {} // 내용값 검증
 }
