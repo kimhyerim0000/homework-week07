72af41ef8ab732fa0d6a2113d68fd33d07d8b044
kimhy <kimhyyefla@gmail.com>
2025-10-19 20:56:10 +0900

    feat: Implement ScheduleBottomSheet component

---
+++ lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -0,0 +1,12 @@
+import 'package:myapp/week07/calendar_scheduler/component/custom_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
+
+class ScheduleBottomSheet extends StatefulWidget {
+  const ScheduleBottomSheet({super.key});
+
+  @override
+  State<ScheduleBottomSheet> createState() => _ScheduleBottomSheetState();
+}
+
+class _Sche
