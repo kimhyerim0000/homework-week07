561a95bae4f531361fc48847bd5d3f1dd2f1ac7f
kimhy <kimhyyefla@gmail.com>
2025-10-19 23:56:05 +0900

    Fix: Replace `dart:id` import with `dart:io`

--- lib/week07/calendar_scheduler/database/drift_database.dart
+++ lib/week07/calendar_scheduler/database/drift_database.dart
@@ -4,7 +4,7 @@ import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
-import 'dart:id';
+import 'dart:io';
 
 // private값까지 불러올 수 있음.
 part 'drift_database.g.dart'; // part 파일 저장
