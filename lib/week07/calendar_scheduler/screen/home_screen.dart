80c586ba248f7f6a394ba17c87ffc96045cababa
kimhy <kimhyyefla@gmail.com>
2025-10-19 21:31:40 +0900

    feat(calendar_scheduler): Implement HomeScreen StatefulWidget with FAB

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -6,3 +6,35 @@ import 'package:myapp/week07/calendar_scheduler/component/main_calendar.dart';
 import 'package:flutter/material.dart';
 
 // StatelessWidget에서 StatefulWidget으로 전환
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
+    // 선택된 날짜를 관리할 변수
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      floatingActionButton: FloatingActionButton( // 새 일정 버튼
+        backgroundColor: PRIMARY_COLOR,
+        onPressed: () {
+          showModalBottomSheet( // BottomSheet 열기
+            context: context,
+            isDismissible: true,  // 배경 탭 했을 때 BottomSheet 닫기
+          )
+        }
+
+      )
+    )
+  }
+}
