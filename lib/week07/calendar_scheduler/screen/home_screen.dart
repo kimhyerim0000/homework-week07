20ce2c25579d6f318a7e06ccdd62ea222e0737f2
kimhy <kimhyyefla@gmail.com>
2025-10-19 21:28:10 +0900

    feat(calendar_scheduler): Convert HomeScreen to StatefulWidget.

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -3,4 +3,6 @@ import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
 import 'package:myapp/week07/calendar_scheduler/component/today_banner.dart';
 import 'package:myapp/week07/calendar_scheduler/component/schedule_card.dart';
 import 'package:myapp/week07/calendar_scheduler/component/main_calendar.dart';
-import 'package:flutter'
+import 'package:flutter/material.dart';
+
+// StatelessWidget에서 StatefulWidget으로 전환
