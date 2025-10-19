0082c9f8b946c7ed3af7199d57ff07f2d33ca14c
kimhy <kimhyyefla@gmail.com>
2025-10-19 21:40:34 +0900

    feat: Add TodayBanner and ScheduleCard to HomeScreen

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -47,6 +47,19 @@ class _HomeScreenState extends State<HomeScreen> {
               selectedDate: selectedDate, // 선택된 날짜 전달하기
               onDaySelected: onDaySelected, // 날짜가 선택됐을 때 실행할 함수
             ),
+            SizedBox(height: 8.0),
+            TodayBanner(
+              // 배너 추가하기
+              selectedDate: selectedDate,
+              count: 0,
+            ),
+            SizedBox(height: 8.0),
+            ScheduleCard(
+              // 구현해둔 일정 카드
+              startTime: 12,
+              endTime: 14,
+              content: '프로그래밍 공부',
+            ),
           ],
         ),
       ),
