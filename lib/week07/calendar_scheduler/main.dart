71891eef3f6ecd625577f364dbc8a43b74349ae5
kimhy <kimhyyefla@gmail.com>
2025-10-19 23:39:36 +0900

    Refactor: Format main.dart for readability.

--- lib/week07/calendar_scheduler/main.dart
+++ lib/week07/calendar_scheduler/main.dart
@@ -3,9 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
+  
   // 플러터 프레임워크가 준비될 때까지 대기
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting(); // intl 패키지 초기화(다국어화)
-  runApp(MaterialApp(home: HomeScreen()));
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
 }
