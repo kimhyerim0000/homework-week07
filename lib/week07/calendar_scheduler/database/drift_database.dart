5de7df18793c8e8090911d9576ec3f75242b6f22
kimhy <kimhyyefla@gmail.com>
2025-10-19 23:53:35 +0900

    feat: Add drift database setup

--- lib/week07/calendar_scheduler/database/drift_database.dart
+++ lib/week07/calendar_scheduler/database/drift_database.dart
@@ -1,6 +1,9 @@
 import 'package:myapp/week07/calendar_scheduler/model/schedule.dart';
 import 'package:drift/drift.dart';
 
+import 'package:drift/native.dart';
+import 'package'
+
 // private값까지 불러올 수 있음.
 part 'drift_database.g.dart'; // part 파일 저장
 
