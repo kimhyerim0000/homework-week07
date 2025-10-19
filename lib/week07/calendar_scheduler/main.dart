28228b605da7013f9491974d536a40dc09ca8c11
kimhy <kimhyyefla@gmail.com>
2025-10-19 23:57:35 +0900

    feat: Register LocalDatabase as a singleton in GetIt

--- lib/week07/calendar_scheduler/main.dart
+++ lib/week07/calendar_scheduler/main.dart
@@ -9,5 +9,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting(); // intl 패키지 초기화(다국어화)
+
+  final database = LocalDatabase(); // 데이터베이스 생성
+
+  GetIt.I.registerSingleton<LocalDatabase>(database);
   runApp(MaterialApp(home: HomeScreen()));
 }
