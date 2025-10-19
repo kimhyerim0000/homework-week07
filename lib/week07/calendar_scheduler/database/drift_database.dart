99ccce869638e745af8a1f82dcf1c7030574685b
kimhy <kimhyyefla@gmail.com>
2025-10-19 23:48:05 +0900

    feat: Initialize Drift database setup

--- lib/week07/calendar_scheduler/database/drift_database.dart
+++ lib/week07/calendar_scheduler/database/drift_database.dart
@@ -5,7 +5,9 @@ import 'package:drift/drift.dart';
 part 'drift_database.g.dart'; // part 파일 저장
 
 @DriftDatabase(
-  tables: [
-    Schedules,
-  ]
+  // 사용할 테이블 등록
+  tables: [Schedules],
 )
+class LocalDatabase extends _$LocalDatabase {
+  // Code Generation으로 생성할 클래스 상속
+}
