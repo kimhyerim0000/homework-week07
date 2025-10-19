93bf09526e51c671b0bf2ef83df83f6ce832a37f
kimhy <kimhyyefla@gmail.com>
2025-10-19 21:45:04 +0900

    feat: Initialize date formatting and Flutter binding

--- lib/week07/calendar_scheduler/main.dart
+++ lib/week07/calendar_scheduler/main.dart
@@ -4,4 +4,7 @@ import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   // 플러터 프레임워크가 준비될 때까지 대기
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializedDateFormatting(); // intl 패키지 초기화(다국어화)
 }
