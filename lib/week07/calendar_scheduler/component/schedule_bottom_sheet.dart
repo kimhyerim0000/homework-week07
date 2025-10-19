cc10afdf8669ec6be340e18d3fc8bc67e6500fb2
kimhy <kimhyyefla@gmail.com>
2025-10-20 00:21:35 +0900

    feat(calendar): Add content validation to schedule bottom sheet

--- lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -114,7 +114,12 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
   }
 
   // 미리 정의해둔 함수
-  String? contentValidator(String? val) { //
+  String? contentValidator(String? val) {
+    // 내용 검증 함수
+    if (val == null || val.length == 0) {
+      return '값을 입력해주세요';
+    }
 
-  } // 내용값 검증
+    return null;
+  } 
 }
