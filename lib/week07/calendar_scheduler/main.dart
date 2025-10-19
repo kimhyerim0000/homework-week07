a066eb641d1c2aa5f71d5f6ea6789957c77089bd
kimhy <kimhyyefla@gmail.com>
2025-10-19 23:57:05 +0900

    feat: Import Drift database and GetIt packages

--- lib/week07/calendar_scheduler/main.dart
+++ lib/week07/calendar_scheduler/main.dart
@@ -1,17 +1,13 @@
-import ''
+import 'package:myapp/week07/calendar_scheduler/database/drift_database.dart';
+import 'package:get_it/get_it.dart';
 import 'package:myapp/week07/calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
-  
   // 플러터 프레임워크가 준비될 때까지 대기
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting(); // intl 패키지 초기화(다국어화)
-  runApp(
-    MaterialApp(
-      home: HomeScreen(),
-    )
-  );
+  runApp(MaterialApp(home: HomeScreen()));
 }
