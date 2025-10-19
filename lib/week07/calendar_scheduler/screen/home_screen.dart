489e2d06988af66c8e8c7c9b67263f7b44f47f2f
kimhy <kimhyyefla@gmail.com>
2025-10-20 00:43:35 +0900

    feat: Display schedules on the home screen

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -60,12 +60,36 @@ class _HomeScreenState extends State<HomeScreen> {
               count: 0,
             ),
             SizedBox(height: 8.0),
-            Expanded( // 남는 공간을 모두 차지
+            Expanded(
+              // 남는 공간을 모두 차지
               // 일정 정보가 Stream으로 제공되기 때문에 StreamBuilder 사용
               child: StreamBuilder<List<Schedule>>(
                 stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
-              )
-
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    // 데이터가 없을 때
+                    return Container();
+                  }
+                  // 화면에 보이는 값들만 렌더링하는 리스트
+                  return ListView.builder(
+                    // 리스트에 입력할 값들의 총 개수
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      // 현재 index에 해당되는 일정
+                      final schedule = snapshot.data![index];
+                      return Padding( // 좌우로 패딩을 추가해서 UI 개선
+                        padding: const EdgeInsets.only(bottom: 8.0, left: 8.0,
+                        right: 8.0),
+                        child: ScheduleCard(
+                          startTime: schedule.startTime,
+                          endTime: schedule.endTime,
+                          content: schedule.content,
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
             ),
             ScheduleCard(
               // 구현해둔 일정 카드
