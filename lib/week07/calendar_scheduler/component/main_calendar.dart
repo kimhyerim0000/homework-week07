6ca03cb56da82550a3783ffab3eaefd1cb3a5edb
kimhyerim <kimhyyefla@gmail.com>
2025-10-19 20:33:53 +0900

    feat: Implement basic main calendar UI

--- lib/week07/calendar_scheduler/component/main_calendar.dart
+++ lib/week07/calendar_scheduler/component/main_calendar.dart
@@ -3,12 +3,35 @@ import 'package:table_calendar/table_calendar.dart';
 import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
 
 class MainCalendar extends StatelessWidget {
-  final OnDaySelected onDaySelected;
-  final DateTime selectedDate;
+  final OnDaySelected onDaySelected; // 날짜 선택 시 실행할 함수
+  final DateTime selectedDate; // 선택된 날짜
 
   const MainCalendar({
     super.key,
     required this.onDaySelected,
     required this.selectedDate,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      locale: 'ko_kr',
+      onDaySelected: onDaySelected, // 날짜 선택 시 실행할 함수
+      selectedDayPredicate:
+          (date) => // 선택된 날짜를 구분할 로직
+              date.year == selectedDate.year &&
+              date.month == selectedDate.month &&
+              date.day == selectedDate.day,
+
+      firstDay: DateTime(1800, 1, 1), // 첫째 날
+      lastDay: DateTime(3000, 1, 1), // 마지막 날
+      focusedDay: DateTime.now(),
+
+      headerStyle: HeaderStyle(
+        titleCentered: true,
+        formatButtonVisible: false,
+        titleTextStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 16.0),
+      ),
+    );
+  }
 }
