1c7122614b5a9683c94c056f50f27ddae5dcf290
kimhyerim <kimhyyefla@gmail.com>
2025-10-19 20:13:58 +0900

    feat: Add CustomTextField component

--- lib/week07/calendar_scheduler/component/custom_text_field.dart
+++ lib/week07/calendar_scheduler/component/custom_text_field.dart
@@ -1,3 +1,10 @@
 import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final bool isTime;
+
+  const CustomTextField({required this.label, required this.isTime, super.key});
+}
