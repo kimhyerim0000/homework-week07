2ad4fab1bb35ea51c27b916269a11b225d170cb3
kimhy <kimhyyefla@gmail.com>
2025-10-19 21:21:10 +0900

    feat: Introduce TodayBanner component with date and count

--- lib/week07/calendar_scheduler/component/today_banner.dart
+++ lib/week07/calendar_scheduler/component/today_banner.dart
@@ -1,2 +1,7 @@
 import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
+
+class TodayBanner extends StatelessWidget {
+  final DateTime selectedDate; // 선택된 날짜
+  final int count; // 일정 개수
+}
